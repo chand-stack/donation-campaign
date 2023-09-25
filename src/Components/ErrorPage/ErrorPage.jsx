2e160faee3b665c440dce76c0c2de552e4b196a5
chand-stack <chand.stack@gmail.com>
2023-09-26 03:24:16 +0600

    Chore: Error Page Added

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,10 @@
-// import errorImg from '../../assets/404design.png'
+import errorImg from '../../assets/404design.png'
 const ErrorPage = () => {
     return (
         <div className='h-screen space-y-2'>
-            
+            <img className='h-[80vh] mx-auto' src={errorImg} alt="" />
+            <h1 className='text-5xl md:text-7xl font-extrabold text-center'>Oooops!!!</h1>
+            <p className='text-3xl md:text-5xl font-extrabold text-center'>The Page You Were Looking For Was Not Found</p>
         </div>
     );
 };
